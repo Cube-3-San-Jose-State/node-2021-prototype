3a55afa7c59ccfc16d0dd23c9dbfa83642666ef4
Nate Garza <nate.garza@hotmail.com>
2021-11-05 14:11:45 -0700

    renames files and removes server upload feature from receiver

---
+++ IoT/Heltec Receiver/src/main.cpp
@@ -0,0 +1,53 @@
+#include "heltec.h"
+#include "LoRa.h"
+#include "HTTPClient.h"
+#include "WiFiMulti.h"
+#include "ArduinoJson.h"
+#include "Adafruit_Sensor.h"
+
+const char *AP_SSID = "kingdom2";
+const char *AP_PWD = "22039622";
+
+String Packet;
+float temperature, humidity, longitude, latitude, smoke;
+
+WiFiMulti wifiMulti;
+
+void LoraRecieve()
+{
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+
+  if (LoRa.available())
+  {
+    Serial.println("Packet Received: ");
+    Packet = LoRa.readString();
+  }
+  Serial.println(Packet);
+  sscanf(Packet.c_str(), "%f\n%f\n%f\n%f\n%f\n", &temperature, &humidity, &longitude, &latitude, &smoke);
+  delay(10000);
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial)
+    ;
+
+  Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Enable*/, false /*Serial Enable*/);
+  Serial.println("LoRa Receiver");
+
+  if (!LoRa.begin(915E6, 1))
+  {
+    Serial.println("Starting LoRa failed!");
+    while (1)
+      ;
+  }
+
+  wifiMulti.addAP(AP_SSID, AP_PWD);
+}
+
+void loop()
+{
+  LoraRecieve();
+}
